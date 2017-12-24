04013be9bd855f5027af0f641979b51ec49aa1b6
Matrix0xCC <mycatclaw@gmail.com>
2017-12-24 17:08:47 +0800

    first commit

---
+++ src/protocol/protocol.go
@@ -0,0 +1,69 @@
+package proto
+
+import "fmt"
+
+type ClientHello struct {
+	version   byte
+	methodLen byte
+	method    byte
+}
+
+type ServerHello struct {
+	version byte
+	method  byte
+}
+
+type ClientCommand struct {
+	Version     byte
+	Command     byte
+	reserved    byte
+	AddressType byte // 1: ipv4, length: 4 ;3:domain name, length:first byte;4: ipv6 length: 16
+	DestAddr    string
+	Port        uint16
+}
+type ServerResponse struct {
+	version     byte
+	reply       byte //connect status: 0, success;1
+	reserved    byte
+	addressType byte
+	bindAddr    [] byte
+	port        uint16
+}
+
+func DecodeClientHello(buffer [] byte) (*ClientHello, error) {
+	if len(buffer) < 3 {
+		return nil, fmt.Errorf("error parsing ClientHello")
+	}
+	var clientHello = ClientHello{version: buffer[0], methodLen: buffer[1], method: buffer[2]}
+	return &clientHello, nil
+}
+
+func DecodeClientCommand(buffer [] byte) (*ClientCommand, error) {
+	if len(buffer) < 5 {
+		return nil, fmt.Errorf("error parsing ClientHello")
+	}
+	var command = ClientCommand{Version: buffer[0], Command: buffer[1], reserved: buffer[2], AddressType: buffer[3]}
+
+	//default set to ipv4
+	var begin = 4
+	var addrLen = 4
+	if command.AddressType == 1 { //ipv4
+		//default
+		command.DestAddr = fmt.Sprintf("%d.%d.%d.%d", buffer[begin], buffer[begin+1],
+			buffer[begin+2], buffer[begin+4])
+
+	} else if command.AddressType == 3 { // domain_name
+		begin = 4 + 1
+		addrLen = int(buffer[4])
+		command.DestAddr = string(buffer[begin: begin+addrLen])
+
+	} else if command.AddressType == 4 { //ipv6
+		addrLen = 16
+		command.DestAddr = fmt.Sprintf("%v", buffer[begin:begin+16])
+	}
+
+	command.DestAddr = string(buffer[begin: begin+addrLen])
+	offset := begin + addrLen
+	command.Port = (uint16(buffer[offset]))<<8 + uint16(buffer[offset+1])
+	return &command, nil
+}
---
+++ src/secure/secure.go
@@ -0,0 +1,49 @@
+package secure
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"net"
+	"io"
+)
+
+type AESEncryptor struct {
+	key string
+	block cipher.Block
+	stream cipher.Stream
+}
+
+func newAesEncryptor() *AESEncryptor {
+	var encryptor = new(AESEncryptor)
+	encryptor.key = "1234567812345678"
+	encryptor.block, _ = aes.NewCipher([]byte(encryptor.key))
+	iv := []byte("1234567812345678")
+	encryptor.stream = cipher.NewCTR(encryptor.block, iv)
+	return encryptor
+}
+
+type Tunnel struct{
+	conn          net.Conn
+	encryptor     *AESEncryptor
+	encryptWriter io.Writer
+	decryptReader io.Reader
+}
+
+func NewSecureTunnel(c net.Conn) *Tunnel {
+	var tunnel = new(Tunnel)
+	tunnel.conn = c //
+	tunnel.encryptor = newAesEncryptor()
+	tunnel.encryptWriter = cipher.StreamWriter{S:tunnel.encryptor.stream, W: tunnel.conn}
+	tunnel.decryptReader = cipher.StreamReader{S:tunnel.encryptor.stream, R: tunnel.conn}
+
+	return tunnel
+}
+
+func (tunnel*Tunnel) Read(p [] byte)(int, error){
+	return tunnel.decryptReader.Read(p)
+}
+
+func (tunnel*Tunnel) Write(p []byte)(int, error){
+	return tunnel.encryptWriter.Write(p)
+}
+
